87b7058e850404306ce583f002363af42b54ccca
wangzhe <1207430398@qq.com>
2017-10-11 17:12:45 +0800

    :clap:

---
+++ src/api/resource.js
@@ -0,0 +1,14 @@
+export const Latest = 'api/news/latest'
+export const LatestDetail = 'api/news'
+//过往消息   11 月 18 日的消息，before 后的数字应为 20131119
+export const SearchNews = 'api/news/before'
+//新闻额外信息
+export const Comments = 'api/story-extra'
+export const ThemesList = 'api/themes'
+export const ThemesDetail = 'api/theme'
+export const LongComments = 'api/story'
+export const ShortComments = 'api/story'
+
+//热门消息
+export const HotNews = 'api/news/hot'
+
